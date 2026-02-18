a1262a8d777fac9db4c5910fa1f3b0368dc3648b
naono <naono@192.168.1.4>
2026-02-18 21:40:17 +0900

    課題19

---
+++ HelloWorld_NaotoKanzaki2/src/Carriculum_dog/dog.java
@@ -0,0 +1,21 @@
+package Carriculum_dog;
+
+public class dog {
+
+
+		// Q1：フィールドに動物の名前の変数を定義してください。
+		String AnimalName;
+		// Q2：フィールドに動物の数の変数を定義してください。
+		int Animalmount;
+		// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+		public dog() {
+			this.AnimalName = "犬";
+		}
+		// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+		public dog(int Animalmount) {
+			this.Animalmount = Animalmount;
+		}
+	}
+	
+
+
