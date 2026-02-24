5426868d669ac5f9723514c551bc2d98290630b4
naono <naono@192.168.1.11>
2026-02-25 01:18:10 +0900

    課題20

---
+++ HelloWorld_NaotoKanzaki2/src/main_20/main_20.java
@@ -0,0 +1,29 @@
+package main_20;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import managing.managing;
+
+public class main_20 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+     //  オブジェクト作成
+      managing m = new managing("日本", "寿司", "和食");
+
+     //  メッセージ出力
+        System.out.println("こんにちは！ここは" + m.countryName + "です！");
+        System.out.println("この" + m.foodName + "はうまい");
+        System.out.println(m.foodName + "は" + m.foodType + "です");
+
+     //日時を出力
+        LocalDateTime now = LocalDateTime.now();
+	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+	    String formattedDate = now.format(formatter);
+	    System.out.println("今の現在の時刻は"+formattedDate+"です。");
+        
+	}
+
+}
---
+++ HelloWorld_NaotoKanzaki2/src/managing/managing.java
@@ -0,0 +1,20 @@
+package managing;
+
+public class managing {
+
+	//国名の変数を定義
+	public String countryName;
+	
+	//食べ物の名前
+    public String foodName;
+    
+    //食べ物の種類
+    public String foodType;
+
+    //コンストラクタを作成
+    public managing(String countryName, String foodName, String foodType) {
+        this.countryName = countryName;
+        this.foodName = foodName;
+        this.foodType = foodType;
+    }
+}
