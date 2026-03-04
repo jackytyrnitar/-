05750e743be9380807e0471382763bf3959e325c
naono <naono@192.168.1.4>
2026-03-04 20:43:26 +0900

    課題19_修正版

--- HelloWorld_NaotoKanzaki2/src/Carriculum_Animal/Animal.java
+++ HelloWorld_NaotoKanzaki2/src/Carriculum_Animal/Animal.java
@@ -8,13 +8,13 @@ public class Animal {
 	public static void main(String[] args) {
 		 // Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		Dog dog1 = new Dog();
-		System.out.println(dog1.AnimalName);	
+		System.out.println(dog1.animalName);	
 	 // Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 		Dog dog2 = new Dog(3);
-		System.out.println(dog2.AnimalAmount);
+		System.out.println(dog2.animalAmount);
 	 // 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 	    LocalDateTime now = LocalDateTime.now();
-	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	    String formattedDate = now.format(formatter);
 	    System.out.println(formattedDate);
 
--- HelloWorld_NaotoKanzaki2/src/Carriculum_Animal/Dog.java
+++ HelloWorld_NaotoKanzaki2/src/Carriculum_Animal/Dog.java
@@ -2,15 +2,15 @@ package Carriculum_Animal;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-			String AnimalName;
+			String animalName;
 			// Q2：フィールドに動物の数の変数を定義してください。
-			int AnimalAmount;
+			int animalAmount;
 			// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 			public Dog() {
-				this.AnimalName = "犬";
+				this.animalName = "犬";
 			}
 			// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
 			public Dog(int AnimalAmount) {
-				this.AnimalAmount = AnimalAmount;
+				this.animalAmount = AnimalAmount;
 			}
 		}
