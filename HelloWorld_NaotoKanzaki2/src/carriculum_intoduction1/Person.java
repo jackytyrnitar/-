f7b6961c222e6a7b8e4533381606f6488f344324
naono <naono@192.168.1.4>
2026-04-18 20:40:00 +0900

    課題31

---
+++ HelloWorld_NaotoKanzaki2/src/carriculum_intoduction1/Person.java
@@ -0,0 +1,15 @@
+package carriculum_intoduction1;
+
+public class Person {
+	// インスタンスフィールドを定義
+		String name;
+		int age;
+		double height;
+
+		// コンストラクタを定義しインスタンスフィールドに値をセット
+		Person(String name, int age, double height) {
+			this.name = name;
+			this.age = age;
+			this.height = height;
+		}
+}
